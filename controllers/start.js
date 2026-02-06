b8a51c30f75055cbacd8351a9fed79c02821c1fe
Isaac Carew <isaac@Mac.vodafone.ultrahub>
2026-02-06 11:58:42 +0000

    Fixing spelling error from previous commit

--- controllers/start.js
+++ controllers/start.js
@@ -6,7 +6,7 @@ import logger from "../utils/logger.js";
 const start = {
   createView(request, response) {
     logger.info("Start page loading!");
-    response.send('Welcome to Isaac;s Playlist app!');   
+    response.send('Welcome to Isaacs Playlist app!');   
   },
 };
 
