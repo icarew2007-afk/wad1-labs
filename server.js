424d33eef47aab08e9436ef22269abb55554a2c8
Isaac Carew <isaac@Mac.vodafone.ultrahub>
2026-02-12 13:41:27 +0000

    Checking handbars

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 'use strict';
 
 import express from 'express';
-import routes from './routes.js';
+import routes from "./routes.js";
 import logger from "./utils/logger.js";
-
+import { create } from 'express-handlebars';
 
 const app = express();
 const port = 3000;
 
-app.use("/",routes);
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
 
+app.use("/", routes);
 
-app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
-
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
